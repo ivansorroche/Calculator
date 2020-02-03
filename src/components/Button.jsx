cd173485f669d5be85dce403ac001418f0fc6bf7
Ivan <ivan@Ivans-MacBook-Pro.local>
2020-02-03 17:07:50 -0300

    Base

---
+++ src/components/Button.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Button.css'
+
+export default props =>  {
+    let classes = "buttom "
+    classes += props.operation ? "operation" : " "
+    classes += props.double ? "double" : " "
+    classes += props.triple ? "triple" : " "
+
+
+     
+    return(
+        <button
+         onClick={e => props.click && props.click(props.label)}
+        className={classes}>
+        {props.label} 
+        </button>
+        )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
