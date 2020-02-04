80b473404c70098c762bd9c37b0a84b01a90e2df
Ivan <ivan@Ivans-MacBook-Pro.local>
2020-02-04 17:00:26 -0300

    Cores nos botoes de operadores

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Button.css'
 
 export default props =>  {
-    let classes = "buttom "
+    let classes = "button "
     classes += props.operation ? "operation" : " "
     classes += props.double ? "double" : " "
     classes += props.triple ? "triple" : " "
